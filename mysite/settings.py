f2017a9035eb3fccd308158a0ddb1b3e9386233d
muhammad_osama_khan <mok232@nyu.edu>
2019-10-11 22:27:23 -0400

    changed static_root in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 # # Line below alone causes error when building with Travis CI
 # import django_heroku
 # django_heroku.settings(locals())
